56738f277beb52add97fc2e471f835ba9eab9a4f
badisbouzayene220-png <badisbouzayene220@gmail.com>
2026-08-08 17:09:12 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-window.BINGO_CONFIG={
-  SUPABASE_URL:"https://ekjrizhsviftjiuumapg.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY:"sb_publishable_duAz3JsRhOyE1oyTlo69SA_R2n1OY84"
-};
+window.BINGO_CONFIG={SUPABASE_URL:"https://ekjrizhsviftjiuumapg.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_duAz3JsRhOyE1oyTlo69SA_R2n1OY84"};
--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,1 @@
-const sb=supabase.createClient(
-  window.BINGO_CONFIG.SUPABASE_URL,
-  window.BINGO_CONFIG.SUPABASE_PUBLISHABLE_KEY
-);
+const sb=window.supabase.createClient(window.BINGO_CONFIG.SUPABASE_URL,window.BINGO_CONFIG.SUPABASE_PUBLISHABLE_KEY);
